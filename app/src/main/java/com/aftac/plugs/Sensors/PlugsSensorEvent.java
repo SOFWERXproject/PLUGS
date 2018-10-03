dc38107d1cdb1ff745131bc3471de6905528438d
AtriusVH <JoshVH87@gmail.com>
2018-10-03 10:31:27 -0400

    Read values from ByteBuffer for standard sensors

--- app/src/main/java/com/aftac/plugs/Sensors/PlugsSensorEvent.java
+++ app/src/main/java/com/aftac/plugs/Sensors/PlugsSensorEvent.java
@@ -1,21 +1,23 @@
 package com.aftac.plugs.Sensors;
 
 import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
 
 // Class to send data to listeners of PlugsSensors
 public class PlugsSensorEvent {
     public ByteBuffer data;
-
-    public int sensorId;
+    
+    public int     sensorId;
     public boolean standardSensor;
-    public int sensorType;
-    public long timestamp;
-    public long utcTimestamp;
-    int accuracy;
+    public int     sensorType;
+    public long    timestamp;
+    public long    utcTimestamp;
+    public int     accuracy;
+    public float[] values;
 
     PlugsSensorEvent(ByteBuffer data) {
         this.data = data;
-
+        
         sensorId = data.getInt();
         standardSensor = ((sensorId & PlugsSensorManager.STANDARD_ANDROID_SENSOR_MASK) != 0);
         sensorId &= ~PlugsSensorManager.STANDARD_ANDROID_SENSOR_MASK;
@@ -23,5 +25,11 @@ public class PlugsSensorEvent {
         timestamp = data.getLong();
         utcTimestamp = data.getLong();
         accuracy = data.getInt();
+        
+        if (standardSensor) {
+            FloatBuffer floatBuf = data.slice().asFloatArray();
+            values = new float[floatBuf.limit() - floatBuf.position()];
+            floatBuf.get[values];
+        }
     }
 }
